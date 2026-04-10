2cf810cc757e7fc1d73d9692e032c4a76ed19d64
bravo-nineteen <my.computer.83@gmail.com>
2026-04-10 10:05:56 +0900

    Create Color.kt

---
+++ app/src/main/java/com/aoj/launcher/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.aoj.launcher.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val TacticalBlack = Color(0xFF090C0A)
+val TacticalOlive = Color(0xFF101711)
+val TacticalPanel = Color(0xCC151D17)
+val TacticalPanelSoft = Color(0x991B241D)
+val TacticalLine = Color(0xFF4B5A47)
+val TacticalKhaki = Color(0xFFC0B07A)
+val TacticalSand = Color(0xFF8A7C56)
+val TacticalText = Color(0xFFE8E6DD)
+val TacticalMutedText = Color(0xFFA4AA9B)
+val TacticalWarning = Color(0xFFB14D3A)
